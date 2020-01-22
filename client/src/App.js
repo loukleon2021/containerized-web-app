ce2d78b3d441129ddd9ad53c8c575d229371776b
loukleon2021 <loukleon2021@gmail.com>
2020-01-21 22:58:15 -0600

    Finished baseline for front end functionality of creation and searching for employees.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, OutlinedInput, Button } from '@material-ui/core'
+
+const myStyles = makeStyles(() => ({
+  root: {
+    position: 'center'
+  },
+  div: {
+    textAlign: 'center'
+  },
+  grid: {
+    display: 'flex',
+  }
+}));
 
 const App = () => {
-  const [initialEmployees, setInitEmployees] = React.useState([]);
+  const classes = myStyles();
   const [employeeList, setEmployeeList] = React.useState([]);
-  
+  const [desiredEmployee, setDesiredEmployee] = React.useState('');
+  const [employeeToAdd, setEmployeeToAdd] = React.useState('');
+  const [employeeCounter, setEmployeeCounter] = React.useState(4);
+  const [searchResults, setSearchResults] = React.useState([]);
   //init employee table
-  useEffect(() => {
-    axios.post(`http://localhost:3001/v1/employees?{'employee_id': 1, 'employee_name': 'JohnBob Jones' }`);
-  }, []);
+  // useEffect(() => {
+  //   axios.post(`http://localhost:3001/v1/employees`,{'employee_id': 2 ,'employee_name':'JohnBob Jones'});
+  // }, []);
   
   
 
@@ -21,50 +38,76 @@ const App = () => {
     });
   }, []);
 
+  const handleSearch = (desiredEmployee) => {
+    setSearchResults(employeeList.filter( employee => employee.employee_name === desiredEmployee));
+  };
+
+
+  const AddEmployee = (employeeToAdd) => {
+    let employeeId = employeeCounter;
+    axios.post(`http://localhost:3001/v1/employees`,{'employee_id': employeeId ,'employee_name': employeeToAdd}).then( res =>
+      {axios.get(`http://localhost:3001/v1/employees`).then((response) => {
+      setEmployeeList(response.data);
+      });})
+      setEmployeeCounter(employeeId++);
+  };
+
+
+  const renderEmployeeList = () => employeeList.map((employee, id) => {
+    return (
+      <div key={id}>
+        {`${employee.employee_name}`}
+        <br />
+      </div>
+    );
+  });
+  
+
+  const renderSearchResults = () => searchResults.map((employee, index) => {
+    return (
+      <div key={index}>
+        {`${employee.employee_name}`}
+        <br />
+      </div>
+    );
+  });
+
 
   return (
-    <div>
-    {employeeList && employeeList.length
-      ? employeeList.map((employee, id) => {
-          return (
-            <div key={id}>
-              {`${employee.name}`}
-              <br />
-            </div>
-          );
-        })
-      : 'No employees present'}
-    </div>
+    <Grid container className={classes.grid}>
+    <Grid item sm={3} className={classes.div}>
+    <h3>Current Employees</h3>
+    {renderEmployeeList()}
+    </Grid>
+    <Grid item sm={6} className={classes.div}>
+      <h1>Welcome to the Employee Directory</h1>
+      <OutlinedInput
+        id="searchBar"
+        variant="filled"
+        value={desiredEmployee}
+        onChange={(e) => setDesiredEmployee(e.target.value)}
+      />
+      <Button
+        id="searchButton"
+        onClick={() => handleSearch(desiredEmployee)}
+        variant="outlined"
+        >Search</Button>
+      
+      <OutlinedInput
+        id="addEmployee"
+        variant="filled"
+        value={employeeToAdd}
+        onChange={(e) => setEmployeeToAdd(e.target.value)}
+      />
+      <Button
+        id="addEmployeeButton"
+        onClick={() => AddEmployee(employeeToAdd)}
+        variant="outlined"
+        >Add Employee</Button>
+        {renderSearchResults()}
+    </Grid>
+    </Grid>
   );
 }
-/*  function App() {
-  const [employeeList, updateEmployeeList] = React.useState([]);
-  React.useEffect(() => {
-    const getEmployeeList = async () => {
-      const response = await fetch(
-        `${process.env.REACT_EMPLOYEES_API}/employees`
-      );
-    const employees = await response.json();
-      if (employees && Array.isArray(employees) && employees.length) {
-        // @ts-ignore
-        updateEmployeeList(employees);
-      }
-    };
-    // getToDoItems();
-  }, []);
-return (
-    <div>
-    {employeeList && employeeList.length
-      ? employeeList.map((employee, id) => {
-          return (
-            <div key={id}>
-              {`${employee.name}`}
-              <br />
-            </div>
-          );
-        })
-      : 'No employees present'}
-    </div>
-  );
-}  */
+
 export default App;
